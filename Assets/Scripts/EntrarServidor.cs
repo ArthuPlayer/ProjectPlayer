f097f534f32e28e6f3700a0f57529b7de138c756
Arthur_Matheus =) <arthurgelenske@gmail.com>
2025-09-24 14:42:44 -0300

    1* commit

---
+++ Assets/Scripts/EntrarServidor.cs
@@ -0,0 +1,17 @@
+using Unity.Netcode;
+
+using UnityEngine;
+
+public class EntrarServidor : MonoBehaviour
+
+{
+
+    public void Entrar()
+
+    {
+
+        NetworkManager.Singleton.StartClient();
+
+    }
+
+}
